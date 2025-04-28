f434670050aaf126252360256d3571a5023eea6e
Abe-xcetch <Kilwakeabe4@gmail.com>
2025-04-28 11:33:31 +0300

    Challenge_one

---
+++ Challenge_One.java
@@ -0,0 +1,14 @@
+// Adds message to the screen
+
+public class Challenge_One {
+
+    void Hello() {
+
+        System.out.println("Hello");
+    }
+
+    public static void main(String[] args) {
+        Challenge_One obj = new Challenge_One();
+        obj.Hello();
+    }
+}
