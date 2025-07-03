72ac3ccddddfdbc44f4a84b75087354a0548de82
Abe-xcetch <Kilwakeabe4@gmail.com>
2025-07-03 20:26:30 +0300

    first commit

---
+++ ExceptionHandling/ArithmeticJava.java
@@ -0,0 +1,24 @@
+package ExceptionHandling;
+
+import java.io.*;
+
+public class ArithmeticJava {
+    public static void main(String[] args) {
+        int number1 = 100;
+        int number2 = 0;
+
+        try {
+            // Code that may throw exception
+            int answer = number1 /  number2;
+            System.out.println("Answer is: " + answer);
+        }
+         catch (ArithmeticException e) {
+
+            //Handling the exception
+             System.out.println("Error: Division by zero is not allowed");
+         }
+        finally {
+            System.out.println("Program continues after handling the exception.");
+        }
+    }
+}
